2f9452ffc5fe83e74596d420f863b864caa34b1b
adithya <adithya.upadhyaya007@gmail.com>
2024-05-19 14:54:28 +0530

    commiting change

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+
+import { useState } from "react";
+
+function App() {
+  const[inputdata,setInputData]=useState("");
+  const[data,setData]=useState([])
+
+  function addItem(){
+      
+    setData([...data,inputdata])
+    setInputData("")
+  }
+
+  return (
+   <div className="container"> 
+      <div className='row mt-5 mx-5'>
+       <div  className='col-lg-4 col-md-4 col-sm-12 ' ></div>
+  
+       <div className=' inputBox row col-lg-4 col-md-4 col-sm-12 '>
+          <div className='col-lg-6 col-sm-6 col-md-6 '>
+               <input 
+               className="form-control"
+               type="text"
+               value={inputdata}
+               onChange={(e)=>setInputData(e.target.value)}
+               />
+          </div>
+          <div  className='col-lg-6 col-sm-6 col-md-6  '>
+          <button className='btn btn-primary' onClick={()=>addItem()} >+</button>
+          </div>
+  
+        </div>
+  
+  
+     
+    </div>
+    <div className="row">
+         <div className="col-lg-3 col-md-12 col-md-4 "></div>
+         <div className="col-lg-6 col-md-12 col-md-4 mx-5 mt-5 ">
+         {data.map((ele)=>{
+                 return(
+                  <div className="container">
+                    <div className="row bg-danger">
+                    <input type="text" className="form-control" value={ele}/>
+                    </div>
+                    <div className="">
+
+                   <span className="btn btn-danger">-</span>
+                    </div>
+                   </div>
+                 )
+               })}
+         </div>
+         <div className="col-lg-3 col-md-12 col-md-4 "></div>
+    </div>
+   
+    </div>
+     
+     
+    
+  )
+}
+
+export default App
